b60b9387b897d95ffcca6a077a89aa31ee645d2d
Anatoly <akiselev57@mail.ru>
2022-07-14 14:51:28 +0300

    2.9. Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+
+const filmDetails = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_DATE: 2014,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={filmDetails.TITLE}
+      genre={filmDetails.GENRE}
+      releaseDate={filmDetails.RELEASE_DATE}
+    />
+
   </React.StrictMode>,
 );
+
+
